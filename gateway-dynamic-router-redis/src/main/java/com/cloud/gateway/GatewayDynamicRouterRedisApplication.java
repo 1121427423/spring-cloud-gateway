a0d282fea9e8073ccaaebdc4846b5e056ee1e91a
king <1121427423@qq.com>
2022-08-28 18:23:44 +0800

    Initial commit

---
+++ gateway-dynamic-router-redis/src/main/java/com/cloud/gateway/GatewayDynamicRouterRedisApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayDynamicRouterRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayDynamicRouterRedisApplication.class, args);
+    }
+
+}
---
+++ gateway-static-router/src/main/java/com/cloud/gateway/GatewayStaticRouterApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author king
+ */
+@SpringBootApplication
+public class GatewayStaticRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayStaticRouterApplication.class, args);
+    }
+
+}
